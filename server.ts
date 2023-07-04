7dfe3f45b2dda7bbb7a0397ce5a2d7b3899724c7
meselghea <122159476+meselghea@users.noreply.github.com>
2023-07-04 17:40:31 +0700

    fix vercel

--- server.ts
+++ server.ts
@@ -1,10 +1,15 @@
 import * as build from "@remix-run/dev/server-build";
-import { createRequestHandler } from "@remix-run/netlify";
 import { installGlobals } from "@remix-run/node";
+import { createRequestHandler } from "@remix-run/vercel";
 
 installGlobals();
 
-export const handler = createRequestHandler({
-  build,
-  mode: process.env.NODE_ENV,
-});
+export default createRequestHandler({
+    build,
+    mode: process.env.NODE_ENV,
+    getLoadContext() {
+      return {
+        nodeLoadContext: true,
+      };
+    },
+  });
